eb104f79a35d1c5aa6197d7e14ec39bfe9462a82
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-01-05 16:24:26 +0530

    Update standardPipeline.groovy

--- var/standardPipeline.groovy
+++ var/standardPipeline.groovy
@@ -24,10 +24,27 @@ def call(Map config) {
         }
         stage('Deploy To Prod') {
           steps {
-            deplyToTomcat(config.prodUrl, config.prodCreds, 'prod-context')
+              parallel(
+                  "Production": {
+                      deployToTomcat(config.prodUrl, config.prodCreds, 'prod-context')
+                  },
+                  "Nexus": {
+                      nexusArtifactUploader(
+                          nexusVersion: 'nexus3', protocol: 'http',
+                          nexusUrl: config.nexusUrl, groupId: config.groupId,
+                          version: config.version, repository: 'maven-releases',
+                          credentialsId: config.nexusCreds,
+                          artifacts: [[artifcatId: config.artifactId, classifier: '', file: "target/${config.artifactId}.war", type: 'war']]
+                      )
+                  }
+              )
           }
         }
         
       }
     }
+}
+
+def deployToTomcat(url, creds, path) {
+    deploy adapters: [tomcat9(credentilasId: creds, url: url)], contextPath: path, war: 'target/*.war'
 }
