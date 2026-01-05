95ffb2c2839b4ccb8a7763d1daf201a132b40d21
Ravikumar-code-maker <ravikumarchinthapally@gmail.com>
2026-01-05 23:15:38 +0530

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -2,11 +2,6 @@ def call(Map config) {
     pipeline {
         agent any
         stages {
-          stage(Checkout) {
-            steps {
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Ravikumar-code-maker/jenkins-shared-library']])
-            }
-          }
             stage('Build') {
                 steps {
                     sh 'mvn clean package'
